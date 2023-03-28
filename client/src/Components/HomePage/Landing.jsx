51a8e64494642b7a464e11b60287d55b771e10a2
chitrakarayat <73735245+chitrakarayat@users.noreply.github.com>
2023-03-28 21:53:08 +0530

    navbar and footer complete

--- client/src/Components/HomePage/Landing.jsx
+++ client/src/Components/HomePage/Landing.jsx
@@ -6,6 +6,13 @@ const Landing = () => {
   return (
     <>
       <Header
+        bgColor="#BBD6B8"
+        title="Invest in Stock Market " 
+        description="Search for your next home with our easy to use search tool."
+        img={logo}
+      />
+
+      <Header
         bgColor="#B2A4FF"
         title="Find your next home"
         description="Search for your next home with our easy to use search tool."
@@ -14,13 +21,6 @@ const Landing = () => {
 
       <Header
         bgColor="#FFEBB7"
-        title="Find your next home"
-        description="Search for your next home with our easy to use search tool."
-        img={logo}
-      />
-
-      <Header
-        bgColor="#BBD6B8"
         title="Find your next home"
         description="Search for your next home with our easy to use search tool."
         img={logo}
