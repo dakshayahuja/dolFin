9c6e195bd3e825708810d2a5b14dd428705cda72
Dakshay <dakshayahuja02@gmail.com>
2023-04-05 19:25:09 +0530

    updated landing page

--- client/src/Components/HomePage/Landing.jsx
+++ client/src/Components/HomePage/Landing.jsx
@@ -1,34 +1,49 @@
 import Header from "../UI/Header";
-import logo from "../../Assets/logo-transparent.png";
+import Stock from "../../Assets/stock.png";
+import Crypto from "../../Assets/crypto.png"
+import MF from "../../Assets/MF.png"
+// import logo from "../../Assets/logo-transparent.png";
 
 const Landing = () => {
   return (
     <>
       <Header
-        bgColor="#BBD6B8"
+        bgColor="#023e8a"
+        textColor="white"
         title="Invest in Stock Markets"
         button="Get Started"
         link="stocks"
         description="Invest smarter with our app's advanced analysis and recommendations."
-        img={logo}
+        img={Stock}
+        className = {"img-fluid"}
+        textPadding="12vw 0 8vw 0"
+        imgPadding="6vw 0"
       />
 
       <Header
-        bgColor="#B2A4FF"
+        bgColor="#e5989b"
         title="Invest in Mutual Funds with Confidence"
         button="Start Investing"
         link="mutualfund"
         description="Take control of your financial future with our app's all-encompassing information on mutual funds."
-        img={logo}
+        img={MF}
+        imgHeight="29vw"
+        margin="0 0 0 6vw"
+        textPadding="11vw 0"
+        imgPadding="6vw 0"
       />
 
       <Header
-        bgColor="#FFEBB7"
+        bgColor="#95d5b2"
         title="Invest in Cryptocurrency - the Future of Money"
         button="Learn More"
         link="crypto"
         description="Get a complete picture of the cryptocurrency market, including prices, volumes, and trends with our app."
-        img={logo}
+        img={Crypto}
+        className = {"img-fluid"}
+        textPadding="10vw 2vw 10vw 0"
+        imgPadding="6vw 0"
+        margin="0 0 0 2.5vw"
       />
     </>
   );
--- client/src/Components/UI/Header.jsx
+++ client/src/Components/UI/Header.jsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 const Header = (props) => {
   return (
       <div
-        className="container-fluid py-5 header"
-        style={{ backgroundColor: props.bgColor }}
+        className="container-fluid header"
+        style={{ backgroundColor: props.bgColor , color: props.textColor}}
       >
-        <div className="row p-5 header-content">
-          <div className="col-7 header-text">
+        <div className="row header-content">
+          <div className="col-8 header-text" style={{padding: props.textPadding}}>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <button className="btn-header" type="btn"><Link to={`/${props.link}`}>{props.button}</Link></button>
           </div>
-          <div className="col-5 header-img">
-            <img src={props.img} alt="img1" className="img-fluid"></img>
+          <div className="col-4 header-img" style={{padding: props.imgPadding}}>
+            <img src={props.img} alt="img1" className={props.className} style={{ height: props.imgHeight, margin: props.margin}}></img>
           </div>
         </div>
       </div>
