414f3411c3abd26e77be4f900cac10b6678a14bd
Dakshay <dakshayahuja02@gmail.com>
2023-04-05 20:13:48 +0530

    mid

--- client/src/Components/HomePage/Landing.jsx
+++ client/src/Components/HomePage/Landing.jsx
@@ -2,14 +2,14 @@ import Header from "../UI/Header";
 import Stock from "../../Assets/stock.png";
 import Crypto from "../../Assets/crypto.png"
 import MF from "../../Assets/MF.png"
-// import logo from "../../Assets/logo-transparent.png";
+import { Fragment } from "react";
 
 const Landing = () => {
   return (
     <>
       <Header
         bgColor="#023e8a"
-        title="Invest in Stock Markets"
+        title={<span>Invest in <span id="head-title">Stock Markets</span></span>}
         button="Get Started"
         link="stocks"
         description="Invest smarter with our app's advanced analysis and recommendations."
@@ -21,7 +21,7 @@ const Landing = () => {
 
       <Header
         bgColor="#037971"
-        title="Invest in Mutual Funds with Confidence"
+        title={<span>Invest in <span id="head-title">Mutual Funds</span> with Confidence</span>}
         button="Start Investing"
         link="mutualfund"
         description="Take control of your financial future with our app's all-encompassing information on mutual funds."
@@ -33,15 +33,15 @@ const Landing = () => {
       />
 
       <Header
-        bgColor="#e5989b"
-        title="Invest in Cryptocurrency - the Future of Money"
+        bgColor="#480ca8"
+        title={<span>Invest in <span id="head-title">Cryptocurrency</span> - the Future of Money</span>}
         button="Learn More"
         link="crypto"
         description="Get a complete picture of the cryptocurrency market, including prices, volumes, and trends with our app."
         img={Crypto}
         className = {"img-fluid"}
         textPadding="10vw 2vw 10vw 0"
-        imgPadding="6vw 0"
+        imgPadding="8vw 0"
         margin="0 0 0 2.5vw"
       />
     </>
