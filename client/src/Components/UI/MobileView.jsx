daf941431dda89198ab134cd81dfdfa17b476859
Dakshay <dakshayahuja02@gmail.com>
2023-04-06 19:54:26 +0530

    hotfix

--- client/src/Components/UI/MobileView.jsx
+++ client/src/Components/UI/MobileView.jsx
@@ -5,14 +5,14 @@ import logo from "../../Assets/logo-light.png";
 
 const MobileView = () => {
   return (
-    <>
+    <div id="mv">
       <img src={logo} alt="logo" id="logo" />
       <h3>Investment solutions that work for you.</h3>
       <h5>Coming soon on</h5>
       <img src={playStore} alt="play_store" style={{ width: "60vw" }}></img>
       <img src={appStore} alt="app_store" style={{ width: "53.75vw" }}></img>
       <p>*Currently supported on desktop browsers only.</p>
-    </>
+    </div>
   );
 };
 
