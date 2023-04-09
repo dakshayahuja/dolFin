89ed6a70f929f0f7ac528d5d4bae19d9b3b715cc
Dakshay <dakshayahuja02@gmail.com>
2023-04-09 10:50:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 
 const app = new express();
 const port = 3000;
-const MONGO_URL =
-  "mongodb+srv://dakshay:dolFin@cluster.aufzslf.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://dakshay:dolFin@cluster.aufzslf.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URL).then(() => {
   console.log("Connected to MongoDB");
