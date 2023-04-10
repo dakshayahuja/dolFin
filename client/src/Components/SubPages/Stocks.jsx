1168a8e341e9025841a6617d12f339d410ce43ac
Dakshay <dakshayahuja02@gmail.com>
2023-04-10 10:56:59 +0530

    trading view widget

--- client/src/Components/SubPages/Stocks.jsx
+++ client/src/Components/SubPages/Stocks.jsx
@@ -1,10 +1,11 @@
 import AppBar from "../Navbar";
 import Footer from "../Footer";
+import TradingView from "../../tradingView";
 export default function Stocks() {
   return (
     <>
       <AppBar />
-      Stocks
+      <TradingView />
       <Footer />
     </>
   );
---
+++ client/src/tradingView.jsx
@@ -0,0 +1,67 @@
+// TradingViewWidget.jsx
+
+import React, { useEffect, useRef } from "react";
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(() => {
+    onLoadScriptRef.current = createWidget;
+
+    if (!tvScriptLoadingPromise) {
+      tvScriptLoadingPromise = new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.id = "tradingview-widget-loading-script";
+        script.src = "https://s3.tradingview.com/tv.js";
+        script.type = "text/javascript";
+        script.onload = resolve;
+
+        document.head.appendChild(script);
+      });
+    }
+
+    tvScriptLoadingPromise.then(
+      () => onLoadScriptRef.current && onLoadScriptRef.current()
+    );
+
+    return () => (onLoadScriptRef.current = null);
+
+    function createWidget() {
+      if (
+        document.getElementById("tradingview_e5d41") &&
+        "TradingView" in window
+      ) {
+        new window.TradingView.widget({
+          width: 1200,
+          height: 600,
+          symbol: "BSE:RELIANCE",
+          interval: "D",
+          timezone: "Etc/UTC",
+          theme: "dark",
+          style: "1",
+          locale: "in",
+          toolbar_bg: "#f1f3f6",
+          enable_publishing: false,
+          withdateranges: true,
+          hide_side_toolbar: false,
+          allow_symbol_change: true,
+          save_image: false,
+          details: true,
+          hotlist: true,
+          show_popup_button: true,
+          popup_width: "1000",
+          popup_height: "650",
+          container_id: "tradingview_e5d41",
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container pt-5">
+      <div id="tradingview_e5d41" />
+    </div>
+  );
+}
